9b300d36e746631f094d0747965e36ec7247525b
Alex <312akim@gmail.com>
2020-07-18 06:15:01 -0500

    Sessions Part 2 & Passport

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const session = require('express-session');
 //Calling returned function from require function
 const FileStore = require('session-file-store')(session);
 
+const passport = require('passport');
+const authenticate = require('./authenticate');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
@@ -52,44 +55,26 @@ app.use(session({
   resave: false, //Once session is created, updated & saved, it will continue to save on requests, even if no updates. Keeps session active/not deleted
   store: new FileStore() //Create new filestore as an object to save session info to hard drive
 }));
+//Only need for session based. Check incom requests for existing session
+//If yes, loads it into req.user
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ** Users can access this without authorization as it's before auth call
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// ** End
 
 //Custom authentication middleware
 function auth(req, res, next) {
-  console.log(req.session); //Created by express session
+  console.log(req.user);
 
-  if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    //If no authentication received
-    if (!authHeader) {
-      const err = new Error('You are not authenticated!');
-      //Lets client know server is requesting authentication & method being requested is basic
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      //Pass error message to Express to send message & authentication request back to client
-      return next(err);
-    }
-    //Parse, format & decode from base64. Final result auth = ["admin", "password"]
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === 'admin' && pass === 'password') {
-              //cookieName//value//option: object with config values. Use secret key from cookieParser to create signed cookie
-      req.session.user = 'admin'; //Sets username as admin in session
-      return next(); //authorized
-    } else {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
+  if (!req.user) {
+      const err = new Error('You are not authenticated!');                    
       err.status = 401;
       return next(err);
-    }
   } else {
-    if (req.session.user === 'admin') {
       return next();
-    } else {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-    }
   }
 }
 
@@ -98,8 +83,6 @@ app.use(auth);
 //Users must authenticate before being able to access below
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
